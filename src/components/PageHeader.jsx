51c407fc2eed2b2078810913a8ed006bafb10ccd
JatinY459 <14jatinyadav@gmail.com>
2025-04-29 11:40:29 +0530

    back icon in services-pg removed

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/pageHeader.css";
 
-const PageHeader = ({ title }) => {
+const PageHeader = ({ title, showBackIcon = true }) => {
   const navigate = useNavigate();
 
   return (
     <div className="page-header">
-      <img
-        src="/images/back-icon.png"
-        alt="Back"
-        className="back-icon"
-        onClick={() => navigate(-1)}
-      />
+      {showBackIcon ? (
+        <img
+          src="/images/back-icon.png"
+          alt="Back"
+          className="back-icon"
+          onClick={() => navigate(-1)}
+        />
+      ) : (
+        <div className="back-icon header-placeholder"></div>
+      )}
+      
       <h2 className="header-title">{title}</h2>
       {/* Invisible icon to balance the layout */}
       <div className="back-icon header-placeholder"></div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -36,7 +36,7 @@ const Services = () => {
 
   return (
     <div className="homepage-container">
-      <PageHeader title="Home Page" />
+      <PageHeader title="Home Page" showBackIcon={false}/>
       <h1 className="main-heading">Welcome to <span className="kanchan">Kanchan</span> <span className="refrigeration">Refrigeration</span></h1>
 
       <hr className="separator-line" />
